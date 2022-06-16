a09461ebaa9965c9ec069474a3a03f8a6c904bd7
Omar Ochoa <omar@ochoawebdesign.com>
2022-06-15 19:37:55 -0700

    Changed var to let or const

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
+    let reveals = document.querySelectorAll(".reveal");
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      const elementVisible = 150;
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
       } else {
